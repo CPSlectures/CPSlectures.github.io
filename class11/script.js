cdcba95f37539d6bc5f275c92f9c8542da38c67d
RobloxJoker <72589257+RobloxJoker@users.noreply.github.com>
2023-05-31 03:43:28 +0530

    f

--- class11/script.js
+++ class11/script.js
@@ -32,42 +32,43 @@
 const dropdown = document.getElementById('dropdown');
 
 function dropdownSort() {
-    fetch('/class11/videos.json')
+  fetch('/class11/videos.json')
     .then(response => response.json())
     .then(data => {
       const videoListContainer = document.getElementById('videoList');
       var chiki = [];
       let html = '';
-      
-        // Iterate through the video data
-        for (let i = 0; i<(data.length); i++){      
-          console.log(data[i])
+      data.sort(function(a,b){return new Date(b.date) - new Date(a.date);});
 
-            if (data[i].chapter == dropdown.value){
-                let s = data[i]
-                chiki.push(s)                
-            }else {
-              console.log("dadsad")
-            }
+      // Iterate through the video data
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i])
 
+        if (data[i].chapter == dropdown.value) {
+          let s = data[i]
+          chiki.push(s)
+        } else {
+          console.log("dadsad")
+        }
+      }
 
-        chiki.forEach(video => {
+      chiki.forEach(video => {
         // Generate HTML for each video link
         html += `
         <div>
-            <h3>${video.title}</h3>
+            <h3>${video.chapter}</h3>
+            <p>Topic: ${video.title}</p>
             <p>Date: ${video.date}</p>
-            <p>Chapter: ${video.chapter}</p>
             <a href="${video.link}">Watch Video</a>
         </div>
         `;
-        });
-    }
+      });
+
       videoListContainer.innerHTML = html;
     })
     .catch(error => {
       console.error('Error fetching video data:', error);
     });
-    
-    console.log(dropdown.value);
+
+  console.log(dropdown.value);
 }
